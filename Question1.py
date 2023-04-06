4e89fae11672e8dc1be0bcabc970d79dfd5abb61
JCMCGroup-Joshua <121637411+JCMCGroup-Joshua@users.noreply.github.com>
2023-04-06 12:51:44 +0100

    Question 1 Completed

--- Question1.py
+++ Question1.py
@@ -1,2 +1,31 @@
 def Run():
-    print("Starting Question 1")
+    print("Starting Question 1 Alt")
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from tqdm import tqdm
+    # Load your dataset
+    df = pd.read_csv('Question1DataAlt.csv')
+
+    # Calculate the correlation between age range and time spent in the hospital
+    corr = None
+    with tqdm(total=2) as pbar:
+        pbar.set_description("Calculating correlation...")
+        corr = df['race'].astype('category').cat.codes.corr(
+            df['time_in_hospital'])
+        pbar.update()
+
+    # Create a bar graph
+    with tqdm(total=1) as pbar:
+        pbar.set_description("Creating bar graph...")
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        ax.bar(df['race'], df['time_in_hospital'], color='blue')
+        ax.set_xlabel('Race')
+        ax.set_ylabel('Time Spent in Hospital')
+        ax.set_title(f'Correlation: {corr:.2f}')
+
+        plt.show()
+        pbar.update()
+
+
+Run()
