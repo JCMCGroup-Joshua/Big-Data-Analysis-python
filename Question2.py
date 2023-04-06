321948e787f508a218e5f8a1d7f1a99a77697482
JCMCGroup-Joshua <121637411+JCMCGroup-Joshua@users.noreply.github.com>
2023-04-06 13:05:20 +0100

    Question 2 Heatmap

--- Question2.py
+++ Question2.py
@@ -2,6 +2,7 @@ def Run():
     print("Starting Question 2")
     import pandas as pd
     import matplotlib.pyplot as plt
+    import numpy as np
     from tqdm import tqdm
     # Load your dataset
     df = pd.read_csv('Question2Data.csv')
@@ -14,15 +15,23 @@ def Run():
             df['time_in_hospital'])
         pbar.update()
 
-    # Create a scatter plot
+    # Create a heatmap
     with tqdm(total=1) as pbar:
-        pbar.set_description("Creating scatter plot...")
+        pbar.set_description("Creating heatmap...")
+        data = pd.pivot_table(df, values='time_in_hospital', index='admission_source_id',
+                              aggfunc=np.mean)
         fig, ax = plt.subplots(figsize=(10, 6))
+        im = ax.imshow(data, cmap='viridis')
 
-        ax.scatter(df['admission_source_id'],
-                   df['time_in_hospital'], color='blue')
+        # Create colorbar
+        cbar = ax.figure.colorbar(im, ax=ax)
+        cbar.ax.set_ylabel("Time Spent in Hospital", rotation=-90, va="bottom")
+
+        # Set tick labels and title
+        ax.set_xticklabels(data.columns)
+        ax.set_yticklabels(data.index)
         ax.set_xlabel('Referal Sauce')
-        ax.set_ylabel('Time Spent in Hospital')
+        ax.set_ylabel('Admission Source ID')
         ax.set_title(f'Correlation: {corr:.2f}')
 
         plt.show()
